62c9b11cc18a2da7ce7e05adc1eb896451efff95
Milena <milena.armada@radiocut.fm>
2026-05-27 16:53:31 -0300

    Agrega endpoint de debug temporal para inspeccionar archivos en Vercel

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,22 @@
+import os
+
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+
+def debug_files(request):
+    base = "/var/task"
+    result = {}
+    for root, dirs, files in os.walk(base):
+        dirs[:] = [d for d in dirs if d not in ("_vendor", "__pycache__", ".git")]
+        rel = os.path.relpath(root, base)
+        result[rel] = files
+    return JsonResponse(result)
+
+
 urlpatterns = [
+    path("__debug__/", debug_files),
     path("admin/", admin.site.urls),
     path("", include("contenidos.urls")),
     path("", include(("usuarios.urls", "usuarios"), namespace="usuarios")),
