133d2b377cf1a9fd3044df1684cf58f812337e44
Milena <milena.armada@radiocut.fm>
2026-06-03 15:44:17 -0300

    Documentar campos de beneficios

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from contenidos.models import Beneficio
+
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
@@ -14,3 +16,17 @@ from django.urls import reverse
 def test_paginas_publicas_responden(client, url_name):
     response = client.get(reverse(url_name))
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_beneficios_publicos_muestran_activos_ordenados(client):
+    Beneficio.objects.create(titulo="Tercero", descripcion="Visible tercero", activo=True, orden=3)
+    Beneficio.objects.create(titulo="Inactivo", descripcion="No visible", activo=False, orden=1)
+    Beneficio.objects.create(titulo="Primero", descripcion="Visible primero", activo=True, orden=1)
+    Beneficio.objects.create(titulo="Segundo", descripcion="Visible segundo", activo=True, orden=2)
+
+    response = client.get(reverse("web:beneficios"))
+
+    contenido = response.content.decode()
+    assert contenido.index("Primero") < contenido.index("Segundo") < contenido.index("Tercero")
+    assert "Inactivo" not in contenido
