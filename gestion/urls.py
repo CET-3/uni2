32dde9a02d95f4a7b7b54a4510cfea7cb33a2736
Milena <milena.armada@radiocut.fm>
2026-06-15 19:57:35 -0300

    Ordenar flujos de asociados y cobros

--- gestion/urls.py
+++ gestion/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import (
     GestionAsociadoDetalleView,
+    GestionAsociadoEditarView,
+    GestionAsociadoNuevoView,
     GestionAsociadosView,
     GestionCobrosView,
     GestionDashboardView,
@@ -20,6 +22,7 @@ app_name = "gestion"
 urlpatterns = [
     path("gestion/", GestionDashboardView.as_view(), name="dashboard"),
     path("gestion/asociados/", GestionAsociadosView.as_view(), name="asociados"),
+    path("gestion/asociados/nuevo/", GestionAsociadoNuevoView.as_view(), name="asociado_nuevo"),
     path("gestion/asociados/exportar.xlsx", GestionExportarAsociadosView.as_view(), name="exportar_asociados"),
     path("gestion/asociados/importar/", GestionImportarAsociadosView.as_view(), name="importar_asociados"),
     path(
@@ -28,6 +31,7 @@ urlpatterns = [
         name="descargar_asociados_revisar",
     ),
     path("gestion/asociados/<int:asociado_id>/", GestionAsociadoDetalleView.as_view(), name="asociado_detalle"),
+    path("gestion/asociados/<int:asociado_id>/editar/", GestionAsociadoEditarView.as_view(), name="asociado_editar"),
     path("gestion/deudores/", GestionDeudoresView.as_view(), name="deudores"),
     path("gestion/cobros/", GestionCobrosView.as_view(), name="cobros"),
     path("gestion/cuotas/importar-historicas/", GestionImportarCuotasHistoricasView.as_view(), name="importar_cuotas_historicas"),
