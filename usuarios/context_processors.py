17d2c34d7fc57e67a4091b7526855e5db6a7705b
Milena <milena.armada@radiocut.fm>
2026-06-15 20:05:23 -0300

    Mejorar menu de usuario

--- usuarios/context_processors.py
+++ usuarios/context_processors.py
@@ -1,4 +1,4 @@
-from .services import user_has_gestion_access, user_is_asociado, user_is_comercio
+from .services import get_available_experiences, user_has_gestion_access, user_is_asociado, user_is_comercio
 
 
 def navigation_roles(request):
@@ -7,6 +7,8 @@ def navigation_roles(request):
     es_comercio = user_is_comercio(user)
     tiene_perfil_asociado = bool(user.is_authenticated and hasattr(user, "asociado"))
     tiene_perfil_comercio = bool(user.is_authenticated and hasattr(user, "comercio"))
+    experiencias_disponibles = get_available_experiences(user)
+    nombre_usuario = user.get_full_name() or user.get_username() if user.is_authenticated else ""
     return {
         "es_asociado": es_asociado,
         "es_comercio": es_comercio,
@@ -14,4 +16,7 @@ def navigation_roles(request):
         "es_staff": bool(user.is_authenticated and user.is_staff),
         "tiene_perfil_asociado": tiene_perfil_asociado,
         "tiene_perfil_comercio": tiene_perfil_comercio,
+        "experiencias_disponibles": experiencias_disponibles,
+        "cantidad_experiencias": len(experiencias_disponibles),
+        "nombre_usuario": nombre_usuario,
     }
