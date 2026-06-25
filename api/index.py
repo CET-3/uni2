9a75b9227d9f96f6f292762e29db41ca1184dc66
Milena <milena.armada@radiocut.fm>
2026-06-24 22:38:52 -0300

    debug: log DATABASE_URL

--- api/index.py
+++ api/index.py
@@ -10,4 +10,11 @@ import django
 
 django.setup()
 
+# DEBUG: obtener DATABASE_URL de producción
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("dbg")
+db_url = os.environ.get("DATABASE_URL", "no-encontrada")
+logger.info("=== DB_URL: %s", db_url)
+
 from config.wsgi import application  # noqa: E402, F401
