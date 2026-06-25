2b7c96ed97c912a42006b1296382729082e20051
Milena <milena.armada@radiocut.fm>
2026-06-24 21:56:06 -0300

    feat: actualiza get_rubros_con_comercios para incluir fotos de comercios

--- comercios/selectors.py
+++ comercios/selectors.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 
 from .models import ActividadComercial, Comercio
 
@@ -12,9 +12,17 @@ def get_comercios_firmados():
 
 
 def get_rubros_con_comercios():
+    comercios_con_foto = Comercio.objects.filter(
+        estado=Comercio.ESTADO_FIRMADO,
+        foto__isnull=False,
+    ).order_by("orden")[:3]
+
     return (
         ActividadComercial.objects
         .filter(comercios__estado=Comercio.ESTADO_FIRMADO)
         .annotate(cantidad_comercios=Count("comercios"))
+        .prefetch_related(
+            Prefetch("comercios", queryset=comercios_con_foto, to_attr="comercios_con_foto")
+        )
         .order_by("nombre")
     )
