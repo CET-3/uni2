8f6ddd5db840089d1e42680b8fdb3d9a7e0d7926
Milena <milena.armada@radiocut.fm>
2026-06-28 22:38:33 -0300

    Ordenar migracion de permiso de design system

--- gestion/migrations/0003_alter_permisogestion_options.py
+++ gestion/migrations/0003_alter_permisogestion_options.py
@@ -54,6 +54,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ("auth", "0012_alter_user_first_name_max_length"),
         ("gestion", "0002_alter_permisogestion_options"),
+        ("usuarios", "0002_crear_grupos_y_permisos"),
     ]
 
     operations = [
